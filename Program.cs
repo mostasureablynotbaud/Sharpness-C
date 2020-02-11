faff4e140768c65a315649b6b52c85599790874c
mostasureablynotbaud <57270428+mostasureablynotbaud@users.noreply.github.com>
2020-02-10 19:13:11 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,57 +1,160 @@
-﻿using static System.Console;
-using System;
+﻿using System;
+using Packt.Shared;
+using static System.Console;
 
-namespace Formating
+namespace PeopleApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int numberOfApples = 12;
-            decimal pricePerApple = 0.35M;
-            Console.WriteLine(
-                format: "{0} apples costs {1:C}",
-                arg0: numberOfApples,
-                arg1: pricePerApple * numberOfApples);
-            string formatted = string.Format(
-                format: "{0} apples costs {1:C}",
-                arg0: numberOfApples,
-                arg1: pricePerApple * numberOfApples);
-            //WriteToFile(formatted); // writes the string into a file
+            var bob = new Person();
+            bob.Name = "Bob Smith";
+            bob.DateOfBirth = new DateTime(1965, 12, 22);
+            WriteLine(
+                format: "{0} was born on {1:dddd, d MMMM yyyy}",
+                arg0: bob.Name,
+                arg1: bob.DateOfBirth);
+            var alice = new Person
+            {
+                Name = "Alice Jones",
+                DateOfBirth = new DateTime(1998, 3, 7)
+            };
+            WriteLine(
+                format: "{0} was born on {1:dd MMM yy}",
+                arg0: alice.Name,
+                arg1: alice.DateOfBirth);
 
-            Console.WriteLine($"{numberOfApples} apples costs {pricePerApple * numberOfApples:C}");
+            bob.FavoriteAncientWonder = 
+                WondersOfTheAncientWorld.StatueOfZeusAtOlympia;
+            WriteLine(format: 
+                "{0}'s favorite wonder is {1}. It's integer is {2}.",
+                arg0: bob.Name,
+                arg1: bob.FavoriteAncientWonder,
+                arg2: (int)bob.FavoriteAncientWonder);
 
-            string applesText = "Apples";
-            int applesCount = 1234;
-            string bananasText = "Bananas";
-            int bananasCount = 56789;
-            Console.WriteLine(
-                format: "{0,-8} {1,6:N0}",
-                arg0: "Name",
-                arg1: "Count");
-            Console.WriteLine(
-                format: "{0,-8} {1,6:N0}",
-                arg0: applesText,
-                arg1: applesCount);
-            Console.WriteLine(
-                format: "{0,-8} {1,6:N0}",
-                arg0: bananasText,
-                arg1: bananasCount);
+            bob.BucketList = 
+                WondersOfTheAncientWorld.HangingGardensOfBabylon
+                | WondersOfTheAncientWorld.MausoleumAtHalicarnassus;
+            // bob.BucketList = (WondersOfTheAncientWorld)18; 
+            WriteLine($"{bob.Name}'s bucket list is {bob.BucketList}");
 
-            Console.Write("Type your first name and press ENTER: "); 
-            string firstName = Console.ReadLine();
-            Console.Write("Type your age and press ENTER: ");
-            string age = Console.ReadLine();
-            Console.WriteLine(
-                $"Hello {firstName}, you look good for {age}.");
+            bob.Children.Add(new Person { Name = "Alfred" }); 
+            bob.Children.Add(new Person { Name = "Zoe" });
+ 
+            WriteLine(
+                $"{bob.Name} has {bob.Children.Count} children:");
+            for (int child = 0; child < bob.Children.Count; child++)
+            {
+                WriteLine($"  {bob.Children[child].Name}");
+            }
+
+            bob.Children.Add(new Person { Name = "Alfred" }); 
+            bob.Children.Add(new Person { Name = "Zoe" });
+ 
+            WriteLine(
+                $"{bob.Name} has {bob.Children.Count} children:");
+            for (int child = 0; child < bob.Children.Count; child++)
+            {
+                WriteLine($"  {bob.Children[child].Name}");
+            }
+
+            BankAccount.InterestRate = 0.012M; // store a shared value
+            var jonesAccount = new BankAccount();
+            jonesAccount.AccountName = "Mrs. Jones";
+            jonesAccount.Balance = 2400;
+            WriteLine(format: "{0} earned {1:C} interest.",
+                arg0: jonesAccount.AccountName,
+                arg1: jonesAccount.Balance * BankAccount.InterestRate);
+            var gerrierAccount = new BankAccount();
+            gerrierAccount.AccountName = "Ms. Gerrier";
+            gerrierAccount.Balance = 98;
+            WriteLine(format: "{0} earned {1:C} interest.",
+                arg0: gerrierAccount.AccountName,
+                arg1: gerrierAccount.Balance * BankAccount.InterestRate);
             
-            Write("Press any key combination: ");
-            ConsoleKeyInfo key = ReadKey();
-            WriteLine();
-            WriteLine("Key: {0}, Char: {1}, Modifiers: {2}",
-            arg0: key.Key,
-            arg1: key.KeyChar,
-            arg2: key.Modifiers);
+            WriteLine($"{bob.Name} is a {Person.Species}");
+
+            WriteLine($"{bob.Name} was born on {bob.HomePlanet}");
+
+            var blankPerson = new Person();
+            WriteLine(format: 
+                "{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}.",
+                arg0: blankPerson.Name,
+                arg1: blankPerson.HomePlanet,
+                arg2: blankPerson.Instantiated);
+
+            var gunny = new Person("Gunny", "Mars");
+            WriteLine(format: 
+                "{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}.",
+                arg0: gunny.Name,
+                arg1: gunny.HomePlanet,
+                arg2: gunny.Instantiated);
+
+            bob.WriteToConsole();
+            WriteLine(bob.GetOrigin());
+
+           (string, int) fruit = bob.GetFruit();
+            WriteLine($"{fruit.Item1}, {fruit.Item2} there are.");
+
+            var fruitNamed = bob.GetNamedFruit();
+            WriteLine($"There are {fruitNamed.Number} {fruitNamed.Name}.");
+
+            var thing1 = ("Neville", 4);
+            WriteLine($"{thing1.Item1} has {thing1.Item2} children.");
+            var thing2 = (bob.Name, bob.Children.Count);
+            WriteLine($"{thing2.Name} has {thing2.Count} children.");
+
+            (string fruitName, int fruitNumber) = bob.GetFruit();
+            WriteLine($"Deconstructed: {fruitName}, {fruitNumber}");
+
+            WriteLine(bob.SayHello());
+            WriteLine(bob.SayHelloTo("Emily"));
+
+            WriteLine(bob.OptionalParameters());
+
+            WriteLine(bob.OptionalParameters("Jump!", 98.5));
+
+            WriteLine(bob.OptionalParameters(
+                number: 52.7, command: "Hide!"));
+
+            WriteLine(bob.OptionalParameters("Poke!", active: false));
+
+            int a = 10;
+            int b = 20;
+            int c = 30;
+            WriteLine($"Before: a = {a}, b = {b}, c = {c}");
+            bob.PassingParameters(a, ref b, out c);
+            WriteLine($"After: a = {a}, b = {b}, c = {c}");
+
+            int d = 10;
+            int e = 20;
+            WriteLine(
+                $"Before: d = {d}, e = {e}, f doesn't exist yet!");
+            // simplified C# 7.0 syntax for the out parameter
+            bob.PassingParameters(d, ref e, out int f);
+            WriteLine($"After: d = {d}, e = {e}, f = {f}");
+
+            var sam = new Person
+            {
+                Name = "Sam",
+                DateOfBirth = new DateTime(1972, 1, 27)
+            };
+            WriteLine(sam.Origin);
+            WriteLine(sam.Greeting);
+            WriteLine(sam.Age);
+
+            sam.FavoriteIceCream = "Chocolate Fudge";
+            WriteLine($"Sam's favorite ice-cream flavor is {sam.FavoriteIceCream}.");
+            sam.FavoritePrimaryColor = "Red";
+            WriteLine($"Sam's favorite primary color is {sam.FavoritePrimaryColor}.");
+
+            sam.Children.Add(new Person { Name = "Charlie" });
+            sam.Children.Add(new Person { Name = "Ella" });
+            WriteLine($"Sam's first child is {sam.Children[0].Name}");
+            WriteLine($"Sam's second child is {sam.Children[1].Name}");
+            WriteLine($"Sam's first child is {sam[0].Name}");
+            WriteLine($"Sam's second child is {sam[1].Name}");
         }
     }
 }
